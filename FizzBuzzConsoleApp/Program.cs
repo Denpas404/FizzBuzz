8482b303bc47e6f42012a890fb0f6b03888a6250
Dennis Paaske <depa@pc-tec.local>
2022-09-14 14:41:08 +0200

    finish push

--- FizzBuzzConsoleApp/Program.cs
+++ FizzBuzzConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FizzBuzzClassLibrary;
 
 namespace FizzBuzzConsoleApp
 {
@@ -10,6 +11,15 @@ namespace FizzBuzzConsoleApp
     {
         static void Main(string[] args)
         {
+            FizzBuzzLogic logic = new FizzBuzzLogic();
+
+            List<int> list = new List<int>() { 3, 5, 15, 98, 10, 4};
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(logic.FizzBuzz(item));
+            }
+            Console.ReadKey();
         }
     }
 }
